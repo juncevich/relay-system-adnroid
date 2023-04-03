e43a2f5aa2595df62a5dd8cf9f101cc88e778d93
Alexandr Juncevich <a.juncevich@gmail.com>
2023-04-04 00:07:07 +0300

    Fix naming

--- app/src/main/java/com/relay/android/MainActivity.kt
+++ app/src/main/java/com/relay/android/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.relay.android.ui.theme.RelaySystemAdnroidTheme
+import com.relay.android.ui.theme.RelaySystemAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RelaySystemAdnroidTheme {
+            RelaySystemAndroidTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    RelaySystemAdnroidTheme {
+    RelaySystemAndroidTheme {
         Greeting("Android 123")
     }
 }
